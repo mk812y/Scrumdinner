42b7c769d47d1e957e83c3b788b665a8faf08f10
mk812y <mk812y@gmail.com>
2022-08-26 13:56:20 +0300

    Initial Commit

---
+++ Scrumdinner/ScrumdinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdinnerApp.swift
+//  Scrumdinner
+//
+//  Created by Михаил Куприянов on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
