4e8456bfef505cfab8438847245f2713c46565ac
mk812y <mk812y@gmail.com>
2022-08-29 21:01:33 +0300

    MeetingView and ScrumsView + UUID

---
+++ Scrumdinner/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinner
+//
+//  Created by Михаил Куприянов on 29.08.2022.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
