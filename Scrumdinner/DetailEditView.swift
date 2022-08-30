1f35c6c5b613b928119dd1f20ca7c498cc7af311
mk812y <mk812y@gmail.com>
2022-08-30 20:24:13 +0300

    TextField

---
+++ Scrumdinner/DetailEditView.swift
@@ -0,0 +1,33 @@
+//
+//  DetailEditView.swift
+//  Scrumdinner
+//
+//  Created by Михаил Куприянов on 30.08.2022.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var data = DailyScrum.Data()
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+                HStack {
+                    Slider(value: $data.lengthInMinutes, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(data.lengthInMinutes)) minutes")
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
