4a7f2d431c9f6d4668d53d1d2f65efd608058d2f
mk812y <mk812y@gmail.com>
2022-09-06 17:42:17 +0300

    HistoryView done

---
+++ Scrumdinner/HistoryView.swift
@@ -0,0 +1,55 @@
+//
+//  HistoryView.swift
+//  Scrumdinner
+//
+//  Created by Михаил Куприянов on 06.09.2022.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    let history: History
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
+                Text("Attendees")
+                    .font(.headline)
+                Text(history.attendeeString)
+                if let transcript = history.transcript {
+                    Text("Transcript")
+                        .font(.headline)
+                        .padding(.top)
+                    Text(transcript)
+                }
+            }
+        }
+        .navigationTitle(Text(history.date, style: .date))
+        .padding()
+    }
+}
+
+extension History {
+    var attendeeString: String {
+        ListFormatter.localizedString(byJoining: attendees.map { $0.name })
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var history: History {
+        History(attendees: [
+            DailyScrum.Attendee(name: "Jon"),
+            DailyScrum.Attendee(name: "Darla"),
+            DailyScrum.Attendee(name: "Luis")
+        ],
+        lenghtInMinutes: 10,
+        transcript: "Darla, would you like to start today? Sure, yesterday I reviewed Luis' RP and met with the design team to finalize the UI..."
+        )
+    }
+    
+    static var previews: some View {
+        HistoryView(history: history)
+    }
+}
