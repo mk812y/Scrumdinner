2f885d123a01489a3c4b252c213f5b2792b75412
mk812y <mk812y@gmail.com>
2022-09-06 17:20:26 +0300

    save

--- Scrumdinner/MeetingView.swift
+++ Scrumdinner/MeetingView.swift
@@ -11,6 +11,8 @@ import AVFoundation
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
     
     private var player: AVPlayer { AVPlayer.sharedDingPlayer }
     
@@ -24,7 +26,7 @@ struct MeetingView: View {
                     secondsRemaining: scrumTimer.secondsRemaining,
                     theme: scrum.theme
                 )
-                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
+                MeetingTimerView(speakers: scrumTimer.speakers, isRecording: isRecording, theme: scrum.theme)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
@@ -39,13 +41,19 @@ struct MeetingView: View {
                 player.seek(to: .zero)
                 player.play()
             }
+            speechRecognizer.reset()
+            speechRecognizer.transcribe()
+            isRecording = true
             scrumTimer.startScrum()
         }
         .onDisappear {
             scrumTimer.stopScrum()
+            speechRecognizer.stopTranscribing()
+            isRecording = false
             let newHistory = History(
                 attendees: scrum.attendees,
-                lenghtInMinutes: scrum.timer.secondsElapsed / 60
+                lenghtInMinutes: scrum.timer.secondsElapsed / 60,
+                transcript: speechRecognizer.transcript
             )
             scrum.history.insert(newHistory, at: 0)
         }
